2dfd9285e658e465197328a4d8f7a379f123be0c
jaerrib <jaerrib@tutanota.com>
2024-01-26 12:36:03 -0500

    Add permissions

--- logs/views.py
+++ logs/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
 
 from .models import Log
+from .permissions import IsInitiatorOrReadOnly
 from .serializers import LogSerializer
 
 
 class LogList(generics.ListCreateAPIView):
+    permission_classes = (IsInitiatorOrReadOnly,)
     queryset = Log.objects.all()
     serializer_class = LogSerializer
 
 
 class LogDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsInitiatorOrReadOnly,)
     queryset = Log.objects.all()
     serializer_class = LogSerializer
